85c1dea927e8f3287f3dd8f7e84190ba85f89dc3
Raghavan <36996970+devyuji@users.noreply.github.com>
2024-11-08 23:56:33 +0530

    removed all side border

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -2,7 +2,7 @@
 	import Container from './container.svelte';
 </script>
 
-<footer class="mt-auto py-4 grid place-items-center border-2 border-zinc-800">
+<footer class="mt-auto py-4 grid place-items-center border-t-2 border-zinc-800">
 	<Container class="grid place-items-center gap-2">
 		<a href="https://github.com/devyuji/ndoujin-web" class="text-zinc-500 text-sm">
 			Source code at github
